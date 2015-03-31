62a977e28ab2d5eafaf72c901763aa99f4841348
Jan Oris <jan.oris@gmail.com>
2015-03-31 15:26:50 +0200

    Changed email to mailto link

--- src/resources/views/people/show.blade.php
+++ src/resources/views/people/show.blade.php
@@ -23,7 +23,7 @@
             </div>
             <div class='col-md-12'>
                 <div class='col-md-4'>{{ Translate::recursive('members.email') }}:</div>
-                <div class='col-md-8'>{{ $person->email }}</div>
+                <div class='col-md-8'>{!! link_to('mailto:'. $person->email, $person->email) !!}</div>
             </div>
             <div class='col-md-12'>
                 <div class='col-md-4'>{{ Translate::recursive('members.cellphone') }}:</div>
