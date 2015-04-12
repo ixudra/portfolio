b3df459e7c4ec08b937712a0561b8aeeacd66fb2
Jan Oris <jan.oris@gmail.com>
2015-04-12 12:56:28 +0200

    Added default package menu structure

---
+++ src/resources/views/layouts/menu-left.blade.php
@@ -0,0 +1,5 @@
+
+    <ul class="nav nav-tabs nav-stacked">
+        <li>{!! HTML::linkRoute('admin.projects.index', Translate::recursive('admin.menu.title.index', array('model' => 'project'))) !!}</li>
+        <li>{!! HTML::linkRoute('admin.customers.index', Translate::recursive('admin.menu.title.index', array('model' => 'customer'))) !!}</li>
+    </ul>
