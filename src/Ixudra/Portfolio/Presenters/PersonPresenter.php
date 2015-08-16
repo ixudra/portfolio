c9e6eef883793209036f116aa7853f0639eccb70
Jan Oris <jan.oris@gmail.com>
2015-08-16 20:24:39 +0200

    Fixed issue with presenter names

--- src/Ixudra/Portfolio/Presenters/PersonPresenter.php
+++ src/Ixudra/Portfolio/Presenters/PersonPresenter.php
@@ -2,10 +2,10 @@
 
 
 use Ixudra\Core\Presenters\BasePresenter;
-use Ixudra\Portfolio\Interfaces\Presenters\CustomerPresenterInterface;
+use Ixudra\Portfolio\Interfaces\Presenters\CustomerPresenterInterface as CustomerPresentationInterface;
 use Ixudra\Portfolio\Presenters\CustomerPresenterInterface as CustomerInterface;
 
-class PersonPresenter extends BasePresenter implements CustomerPresenterInterface, CustomerInterface {
+class PersonPresenter extends BasePresenter implements CustomerPresentationInterface, CustomerInterface {
 
     public function fullName()
     {
