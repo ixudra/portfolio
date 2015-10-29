edbd357e6ea3a6a65c1e8c3efed60de8c49c993f
Jan Oris <jan.oris@gmail.com>
2015-10-29 14:45:12 +0100

    Fixed view error

--- src/resources/views/companies/form/full.blade.php
+++ src/resources/views/companies/form/full.blade.php
@@ -1,13 +1,13 @@
 
     @include('portfolio::companies.form.fields', array('prefix' => $prefix))
 
-    <div class=row">
+    <div class="row">
         <h3>{{ Translate::recursive('portfolio::members.corporateAddress') }}</h3>
     </div>
 
     @include('portfolio::addresses.fields', array('prefix' => 'corporate_address_'))
 
-    <div class=row">
+    <div class="row">
         <h3>{{ Translate::recursive('portfolio::members.representative') }}</h3>
     </div>
 
